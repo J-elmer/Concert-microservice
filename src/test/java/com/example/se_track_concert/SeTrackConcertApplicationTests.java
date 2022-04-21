983a5c775dc0ee8c989f9da792ace81a06fae21a
Jelmer Dijkstra <jelmer.dijkstra@capgemini.com>
2022-04-21 11:30:29 +0200

    added test profile

--- src/test/java/com/example/se_track_concert/SeTrackConcertApplicationTests.java
+++ src/test/java/com/example/se_track_concert/SeTrackConcertApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.se_track_concert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest
-//class SeTrackConcertApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+@SpringBootTest
+@ActiveProfiles("test")
+class SeTrackConcertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
